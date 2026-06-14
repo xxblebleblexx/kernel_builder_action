9f8941612da56b3291ad6668bfa62136c6cf6f1b
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-06-14 17:54:03 +0700

    Modify defconfig_path comment in kernel.sh
    Updated defconfig_path comment to indicate no need for editing.

--- kernel.sh
+++ kernel.sh
@@ -2,7 +2,7 @@
 kernelsource=https://android.googlesource.com/kernel/common # Must be edited
 kernelname=$(basename "$kernelsource" .git) # No need to edit
 branch_kernel=android15-6.6-lts # Must be edited
-defconfig_path=arch/arm64/configs/gki_defconfig # Must be edited
+defconfig_path=arch/arm64/configs/gki_defconfig # No need to edit
 defconfig=${defconfig_path##*/}
 fast_path=$GITHUB_WORKSPACE # This where kernelsource saved
 
