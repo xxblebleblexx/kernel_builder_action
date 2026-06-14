e8f79d5d4f10ae31eea773a49b9e61d6a995914e
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-06-14 17:51:56 +0700

    Modify kernel.sh for Android kernel configuration
    Updated kernel source and branch for Android kernel build. Changed KSU driver setup script and removed manual hook configuration.

--- kernel.sh
+++ kernel.sh
@@ -1,33 +1,18 @@
 #CONFIGURATION
-kernelsource=https://github.com/xxblebleblexx/moonbeam_gale_kernel.git # Must be edited
+kernelsource=https://android.googlesource.com/kernel/common # Must be edited
 kernelname=$(basename "$kernelsource" .git) # No need to edit
-branch_kernel=moonbeam # Must be edited
-defconfig_path=arch/arm64/configs/gale_defconfig # Must be edited
+branch_kernel=android15-6.6-lts # Must be edited
+defconfig_path=arch/arm64/configs/gki_defconfig # Must be edited
 defconfig=${defconfig_path##*/}
 fast_path=$GITHUB_WORKSPACE # This where kernelsource saved
-hooks=manual #only manual hook/kprobes hook, must be edited
 
 cd $fast_path
 git clone -b $branch_kernel --depth=1 $kernelsource;wait
 cd $fast_path/$kernelname
 
 #KSU DRIVER
-curl -LSs "https://raw.githubusercontent.com/xxblebleblexx/MultiSU/refs/heads/legacy/kernel/setup.sh" | bash -s legacy
-
+curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s stable
 #KSU ACTIVATION
 echo "CONFIG_KSU=y" >> $defconfig_path
-
-if [ "$hooks" = "kprobes" ]; then
-#KPROBES HOOK
-echo "CONFIG_KPROBES=y" >> $defconfig_path
-echo "CONFIG_KPROBE_EVENTS=y" >> $defconfig_path
-echo "CONFIG_KSU_KPROBES_HOOK=y" >> $defconfig_path
-fi
-
-if [ "$hooks" = "manual" ]; then
-#MANUAL HOOK
-echo "CONFIG_KSU_MANUAL_HOOK=y" >> $defconfig_path
-wget https://raw.githubusercontent.com/xxblebleblexx/manual_hook_fix/refs/heads/main/manualhook_1.6_fixed.patch;wait;patch -p1 < manualhook_1.6_fixed.patch
-fi
-
+#Compile
 make O=out ARCH=arm64 $defconfig; printf "Y\n2\n\n\n\nY\n" | make -j$(nproc --all) CC=clang O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu-
