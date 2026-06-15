2c2dc8c88dc468d8bc2fd57552e5b8225581fc39
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-06-15 17:51:40 +0700

    Add sudo to commands in kernel.sh
    Updated kernel.sh to use sudo for commands requiring elevated privileges.

--- kernel.sh
+++ kernel.sh
@@ -13,24 +13,24 @@ cd $fast_path
 #download kernel source from aosp
 sudo repo init -u $kernelsource -b $branch_kernel --depth=1 ;wait;sudo repo sync -c -j$(nproc) --no-clone-bundle --no-tags
 
-cd common
+sudo cd common
 #Set name for linux kernel
-echo "CONFIG_LOCALVERSION=\"-$kernelname-LTS\"" >> $defconfig_path
-echo "CONFIG_LOCALVERSION_AUTO=n" >> $defconfig_path
+sudo echo "CONFIG_LOCALVERSION=\"-$kernelname-LTS\"" >> $defconfig_path
+sudo echo "CONFIG_LOCALVERSION_AUTO=n" >> $defconfig_path
 #KSU DRIVER
-curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s stable
+sudo curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s stable
 #KSU ACTIVATION
-echo "CONFIG_KSU=y" >> $defconfig_path
+sudo echo "CONFIG_KSU=y" >> $defconfig_path
 #disable post_defconfig
-sed -i 's/POST_DEFCONFIG_CMDS="check_defconfig"/POST_DEFCONFIG_CMDS=""/g' build.config.gki
+sudo sed -i 's/POST_DEFCONFIG_CMDS="check_defconfig"/POST_DEFCONFIG_CMDS=""/g' build.config.gki
 #disable abi export protection
-sed -i 'd' android/abi_gki_protected_exports_aarch64
+sudo sed -i 'd' android/abi_gki_protected_exports_aarch64
 #Compile
 cd ../
 if [[ "$compile_type" =~ ^android(13|14|15|16)$ ]]; then
-tools/bazel build --config=fast //common:kernel_aarch64_dist
+sudo tools/bazel build --config=fast //common:kernel_aarch64_dist
 fi
 
 if [ "$compile_type" = "android12" ]; then
-LTO=thin BUILD_CONFIG=common/build.config.gki.aarch64 build/build.sh
+sudo LTO=thin BUILD_CONFIG=common/build.config.gki.aarch64 build/build.sh
 fi
