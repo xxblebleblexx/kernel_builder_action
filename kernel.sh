2cec13e024a9316e48d23eac35479aec97c6ba08
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-06-15 20:23:09 +0700

    Refactor kernel.sh to remove unnecessary sudo commands
    Removed 'sudo' from various commands in kernel.sh to allow execution without elevated privileges.

--- kernel.sh
+++ kernel.sh
@@ -11,26 +11,26 @@ compile_type=${helper%%-*}
 mkdir -p gki
 cd $fast_path
 #download kernel source from aosp
-sudo repo init -u $kernelsource -b $branch_kernel --depth=1 ;wait;sudo repo sync -c -j$(nproc) --no-clone-bundle --no-tags
+repo init -u $kernelsource -b $branch_kernel --depth=1 ;wait;sudo repo sync -c -j$(nproc) --no-clone-bundle --no-tags
 
-sudo cd common
+cd common
 #Set name for linux kernel
-sudo echo "CONFIG_LOCALVERSION=\"-$kernelname-LTS\"" >> $defconfig_path
-sudo echo "CONFIG_LOCALVERSION_AUTO=n" >> $defconfig_path
+echo "CONFIG_LOCALVERSION=\"-$kernelname-LTS\"" >> $defconfig_path
+echo "CONFIG_LOCALVERSION_AUTO=n" >> $defconfig_path
 #KSU DRIVER
-sudo curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s stable
+curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s stable
 #KSU ACTIVATION
-sudo echo "CONFIG_KSU=y" >> $defconfig_path
+echo "CONFIG_KSU=y" >> $defconfig_path
 #disable post_defconfig
-sudo sed -i 's/POST_DEFCONFIG_CMDS="check_defconfig"/POST_DEFCONFIG_CMDS=""/g' build.config.gki
+sed -i 's/POST_DEFCONFIG_CMDS="check_defconfig"/POST_DEFCONFIG_CMDS=""/g' build.config.gki
 #disable abi export protection
-sudo sed -i 'd' android/abi_gki_protected_exports_aarch64
+sed -i 'd' android/abi_gki_protected_exports_aarch64
 #Compile
 cd ../
 if [[ "$compile_type" =~ ^android(13|14|15|16)$ ]]; then
-sudo tools/bazel build --config=fast //common:kernel_aarch64_dist
+tools/bazel build --config=fast //common:kernel_aarch64_dist
 fi
 
 if [ "$compile_type" = "android12" ]; then
-sudo LTO=thin BUILD_CONFIG=common/build.config.gki.aarch64 build/build.sh
+LTO=thin BUILD_CONFIG=common/build.config.gki.aarch64 build/build.sh
 fi
