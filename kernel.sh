baf6b6f4a7f0894fcd0bbd80b0c77e9bd0b67425
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-06-15 17:39:04 +0700

    Refactor kernel.sh for improved build process
    Updated kernel.sh to change kernel source URL, adjust fast_path, and modify build commands based on compile type.

--- kernel.sh
+++ kernel.sh
@@ -1,15 +1,18 @@
 #CONFIGURATION
-kernelsource=https://android.googlesource.com/kernel/common # Must be edited
+kernelsource=https://android.googlesource.com/kernel/manifest # No need to edit
 kernelname=Galactic #Must be edited
 branch_kernel=android15-6.6-lts # Must be edited
 defconfig_path=arch/arm64/configs/gki_defconfig # No need to edit
 defconfig=gki_defconfig
-fast_path=$GITHUB_WORKSPACE # This where kernelsource saved
+fast_path=$GITHUB_WORKSPACE/gki # This where kernelsource saved
+compile_type=${branch_kernel%%-*}
 
+mkdir -p gki
 cd $fast_path
-git clone -b $branch_kernel --depth=1 $kernelsource $kernelname ;wait
-cd $fast_path/$kernelname
+#download kernel source from aosp
+repo init -u $kernelsource -b $branch_kernel --depth=1 ;wait;repo sync -c -j$(nproc) --no-clone-bundle --no-tags
 
+cd common
 #Set name for linux kernel
 echo "CONFIG_LOCALVERSION=\"-$kernelname-LTS\"" >> $defconfig_path
 echo "CONFIG_LOCALVERSION_AUTO=n" >> $defconfig_path
@@ -17,5 +20,16 @@ echo "CONFIG_LOCALVERSION_AUTO=n" >> $defconfig_path
 curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s stable
 #KSU ACTIVATION
 echo "CONFIG_KSU=y" >> $defconfig_path
+#disable post_defconfig
+sed -i 's/POST_DEFCONFIG_CMDS="check_defconfig"/POST_DEFCONFIG_CMDS=""/g' build.config.gki
+#disable abi export protection
+sed -i 'd' android/abi_gki_protected_exports_aarch64
 #Compile
-make O=out ARCH=arm64 $defconfig; printf "Y\n\n\n\n\n\n3\n\n\n\n" | make -j$(nproc --all) CC=clang O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu-
+cd ../
+if [[ "$compile_type" =~ ^android(13|14|15|16)$ ]]; then
+tools/bazel build --config=fast //common:kernel_aarch64_dist
+fi
+
+if [ "$compile_type" = "android12" ]; then
+LTO=thin BUILD_CONFIG=common/build.config.gki.aarch64 build/build.sh
+fi
